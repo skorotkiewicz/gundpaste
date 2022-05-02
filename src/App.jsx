be1aa7d68064f00c713b1a50b89b647febca4929
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2022-05-02 03:10:36 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import "./App.scss";
+import Gun from "gun/gun";
+import { nanoid } from "nanoid";
+
+let gun = Gun();
+gun.opt({
+  peers: [
+    "https://grizzly.de1.hashbang.sh/gun",
+    // "https://gun-manhattan.herokuapp.com/gun",
+  ],
+});
+
+function App() {
+  let params = useParams();
+  let navigate = useNavigate();
+  const [bin, setBin] = useState(params.binId);
+  const [bins, setBins] = useState([]);
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    if (!bin) return;
+
+    gun
+      .get("grizzly")
+      .get("bin")
+      .get(bin)
+      .on((data, key) => {
+        console.log(data);
+        setData(data);
+      });
+  }, [bin]);
+
+  useEffect(() => {
+    if (!bin) return;
+
+    const timeout = setTimeout(() => {
+      gun.get("grizzly").get("bin").get(bin).put(data);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [bin, data]);
+
+  const newId = () => {
+    const id = nanoid(10);
+    setBin(id);
+    setData("");
+    navigate("/bin/" + id);
+    return id;
+  };
+
+  return (
+    <div className="container">
+      <header>~decentralized pastebin</header>
+      <main>
+        <div className="area">
+          <textarea
+            value={data}
+            placeholder="paste here!"
+            onChange={(e) => {
+              setData(e.target.value);
+
+              if (!params.binId) {
+                const id = newId();
+                gun.get("grizzly").get("bin").get(id).put(e.target.value);
+                setBins((prev) => [id, ...prev]);
+              }
+            }}
+          ></textarea>
+        </div>
+        <div className="side">
+          <button
+            onClick={() => {
+              setBins((prev) => [newId(), ...prev]);
+            }}
+          >
+            Add new bin
+          </button>
+
+          <div className="bins">
+            {bins.map((b, k) => (
+              <div
+                className="bin"
+                key={k}
+                onClick={() => {
+                  setBin(b);
+                  navigate("/bin/" + b);
+                }}
+              >
+                {b}
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
+      {/* <footer>Footer</footer> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          {/* <Route path="expenses" element={<App />} /> */}
+
+          <Route path="bin" element={<App />}>
+            <Route path=":binId" element={<App />} />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
