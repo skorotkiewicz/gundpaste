d03d4f9b0f46f66d6a933611e10c17c8e7c55f2c
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2022-05-05 00:30:16 +0200

    update sass and remove index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Gun from "gun/gun";
